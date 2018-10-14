e2b2fe9fe43576537f8a3e480a525ea34fa8aba1
Agustin Sandoval <ant.qa3@gmail.com>
2018-10-14 12:21:54 -0500

    Correos

--- pofiles/pro/enviar.php
+++ pofiles/pro/enviar.php
@@ -112,7 +112,7 @@ $mail->addReplyTo('reply-box@hostinger-tutorials.com', 'Your Name');
 $mail->addAddress('guty.boo69@gmail.com', 'Agustin');
 $mail->Subject = 'ConectovoMX Trabajo solicitado';
 //$mail->msgHTML(file_get_contents('message.html'), __DIR__);
-$mail->AltBody = 'Hola buen dia me interesa tu trabajo';
+$mail->Body = 'Hola buen dia me interesa tu trabajo';
 //$mail->addAttachment('test.txt');
 if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
