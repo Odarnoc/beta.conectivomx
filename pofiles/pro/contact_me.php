c4b990ab40797b6222c8dc928b226a7f4999a584
Agustin Sandoval <ant.qa3@gmail.com>
2018-10-13 22:27:12 -0500

    Otra vez con los mails

--- pofiles/pro/contact_me.php
+++ pofiles/pro/contact_me.php
@@ -11,10 +11,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = "guty.boo69@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Ha sido contactado por:  $name";
+$body = "Tienes un mensaje nuevo desde ConecticoMX.\n\n"."Aqui los detalles:\n\nNombre: $name\n\nCorreo: $email\n\nTelefono: $phone\n\nMensaje:\n$message";
+$header = "From: noreply@conectivomx.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header)){
