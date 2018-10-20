261de5274c69e8bb234035edfd571b704a965171
Agustin Sandoval <ant.qa3@gmail.com>
2018-10-20 18:25:33 -0500

    cambios en el apartado conocenos

--- conocenos.php
+++ conocenos.php
@@ -10,28 +10,8 @@
         include 'header.php';
     ?>
 
-    <div class="wrap" style="margin-top:150px;margin-bottom:20px;">
-        <div class="container">
-            <section>
-                <div style="text-align: justify; font-size:18px; " class="row">
-                    <div class="span4"style="margin-left:15px;margin-right:15px;">
-                        <h2 style="text-align:center;" class="title"><span>Misión</span></h2>
-                        <h4 style="font-weight: normal;"> Ayudar a las empresas a obtener más alcance y nuevos clientes.</h4>
-                    </div>
-                    <div class="span4" style="margin-left:15px;margin-right:15px;">
-                        <h2 style="text-align:center;" class="title">Visión</h2>
-                        <h4 style="font-weight: normal;">Ser la plataforma más utilizada en los negocios digitales.</h4>
-                    </div>
-                    <div class="span4" style="margin-left:15px;margin-right:15px;">
-                        <h2 style="text-align:center;" class="title"><span>ConectivoMX</span></h2>
-                        <h4 style="font-weight: normal;">Somos una empresa dedicada al <b>Negocio digital</b> ofreciendo nuestra plataforma para aumentar el <b>alcance</b> y <b>crecimiento empresarial</b> así como <b>facilitar la búsqueda</b> de nuestros usuarios, logrando <b>Negocios reales</b> en línea. </h4>
-                        <h4 style="font-weight: normal;">Aquí podrás encontrar cualquier tipo de negocio en <b>todo México</b> a tu alcance.</h4>
-
-                    </div>
-                </div>
-            </section>
-
-        </div>
+    <div style="margin-top:120px">
+        <img style="width:100%;" src="img/bg-img/myv.jpg">
     </div>
 
     <?php
